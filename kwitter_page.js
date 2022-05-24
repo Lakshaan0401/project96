8065012f7b9b2537f0c1b9a16af0b2fc55f1de29
Lakshaan0401 <99709495+Lakshaan0401@users.noreply.github.com>
2022-05-24 11:04:17 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,44 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyCEqyeIh15yQnfvzWCo3y3xv-LXEVHcyJc",
+      authDomain: "project94-43344.firebaseapp.com",
+      databaseURL: "https://project94-43344-default-rtdb.firebaseio.com",
+      projectId: "project94-43344",
+      storageBucket: "project94-43344.appspot.com",
+      messagingSenderId: "346275357071",
+      appId: "1:346275357071:web:4c42df54fc44588ba6abfa"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
+function send()
+{
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0 
+      });
+      document.getElementById("msg").value=""; 
+}
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html"
+}
